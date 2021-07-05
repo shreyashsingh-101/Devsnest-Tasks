7071570bc3c3c561830a138100079a6a9d968472
Shreyash Singh <Rohitsingh9450@hotmail.com>
2021-07-05 15:11:28 +0530

    Day 14

---
+++ THA Day 14/script.js
@@ -0,0 +1,52 @@
+// Event Listeners
+
+
+// onClick
+document.querySelector('button').addEventListener('click', () =>{
+    console.log("Submit Clicked!!")
+})
+
+// onChange
+document.querySelector('input').addEventListener('change', () =>{
+    console.log("Input Changed!!")
+})
+
+// keyDown
+document.querySelector('input').addEventListener('keydown', () =>{
+    console.log("Key Pressed!!")
+})
+
+// keyUp
+document.querySelector('input').addEventListener('keyup', () =>{
+    console.log("Key Released!!")
+})
+
+// Double Click
+document.querySelector('button').addEventListener('dblclick', () =>{
+    console.log("Submit Double Clicked!!")
+})
+
+// Focus Lost
+document.querySelector('button').addEventListener('blur', () =>{
+    console.log("Focus lost!!")
+})
+
+// Focus Gained
+document.querySelector('input').addEventListener('focus', () =>{
+    console.log("Focus Gained!!")
+})
+
+// mouse over
+document.querySelector('input').addEventListener('mouseover', () =>{
+    console.log("Mouse moved over!!")
+})
+
+// mouse out
+document.querySelector('input').addEventListener('mouseout', () =>{
+    console.log("Mouse moved out!!")
+})
+
+
+
+
+
